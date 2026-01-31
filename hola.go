db0ce4eceeae3491c5f2ccc6f6c67edaf3959f5b
Andrés Frías <inf512.andres.frias@gmail.com>
2026-01-30 23:29:20 -0400

    Update hola.go

--- hola.go
+++ hola.go
@@ -6,34 +6,16 @@ import (
 	"rsc.io/quote"
 )
 
-// Declaracion de constantes
-const Pi float32 = 3.141592653589793
-
-const (
-	x = 100
-	y = 0b100
-	z = 0o12
-	w = 0xFF
-)
-
-const (
-	Domingo = iota + 1
-	Lunes
-	Martes
-	Miercoles
-	Jueves
-	Viernes
-	Sabado
-)
-
 func main() {
 	fmt.Println("Hola Mundo")
 	fmt.Println(quote.Go())
 
-	// Declaracion de variables
-	var firstName, lastName, age = "Jane", "Doe", 23
+	fullName := "Andres Frias \t(alias \"afriasdev\")\n"
+	fmt.Print(fullName)
 
-	fmt.Println(firstName, lastName, age)
+	var a byte = 'A'
+	fmt.Println(a)
 
-	fmt.Println(x, w, y, z)
+	s := "Hola"
+	fmt.Println(s[0])
 }
