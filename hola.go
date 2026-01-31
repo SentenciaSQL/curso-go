83d14a869480c2b6dd136ff0311f6a61f5a53d0f
Andrés Frías <inf512.andres.frias@gmail.com>
2026-01-30 23:33:10 -0400

    Update hola.go

--- hola.go
+++ hola.go
@@ -6,34 +6,18 @@ import (
 	"rsc.io/quote"
 )
 
-// Declaracion de constantes
-const Pi float32 = 3.141592653589793
-
-const (
-	x = 100
-	y = 0b100
-	z = 0o12
-	w = 0xFF
-)
-
-const (
-	Domingo = iota + 1
-	Lunes
-	Martes
-	Miercoles
-	Jueves
-	Viernes
-	Sabado
-)
-
 func main() {
 	fmt.Println("Hola Mundo")
 	fmt.Println(quote.Go())
 
-	// Declaracion de variables
-	var firstName, lastName, age = "Jane", "Doe", 23
+	var (
+		defaultInt     int
+		defaultUint    uint
+		defaultFloat   float32
+		defaultBoolean bool
+		defaultString  string
+	)
 
-	fmt.Println(firstName, lastName, age)
+	fmt.Println(defaultInt, defaultUint, defaultFloat, defaultBoolean, defaultString)
 
-	fmt.Println(x, w, y, z)
 }
